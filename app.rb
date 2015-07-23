22f56550060fefc5025ac9e5be6888bd5719e0c6
Emanuel Comsa <cremzinc@gmail.com>
2015-07-23 22:48:09 +0300

    Added remote call for the generate path.

--- app.rb
+++ app.rb
@@ -12,7 +12,26 @@ Dir[File.dirname(__FILE__) + '/controllers/*.rb'].each { |file| require file }
 
 # @class FiberChallenge
 class FyberChallenge < Sinatra::Base
+  register Sinatra::ConfigFile
+
+config_file './config.yml'
+
   get '/' do
     erb :index
+  end
+
+  post '/generate' do
+    puts settings
+    build_params = {
+      appid:        settings.appid,
+      format:       settings.format,
+      device_id:    settings.device_id,
+      locale:       settings.locale,
+      ip:           settings.ip,
+      offer_types:  settings.offer_types,
+      api_key:      settings.api_key
+    }.merge(params)
+    content_type :json
+    build_params.to_json
   end
 end
